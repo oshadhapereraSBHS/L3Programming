2fe65c408a0ea5948737880f106f13a4af7b27c0
oshadhapereraSBHS <80649645+oshadhapereraSBHS@users.noreply.github.com>
2023-09-19 13:09:24 +1200

    adding all metadata

---
+++ metadata.js
@@ -0,0 +1,198 @@
+
+const Item1 = new Item (
+    "/images/museum (1).jpg",
+    1,
+    "World War I Plaque",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Gallipoli","War"],
+    "New Zealand",
+    "War",
+    "1981",
+    "2009",
+    "Tom Smith",
+);
+
+
+const Item2 = new Item (
+    "/images/museum (2).jpg",
+    2,
+    "Stained Glass Painting",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Art","Stained glass"],
+    "New Zealand",
+    "Art",
+    "1941",
+    "1988",
+    "Frank Peters",
+);
+
+const Item3 = new Item (
+    "/images/museum (3).jpg",
+    3,
+    "D Grant, SBHS Headmaster",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["War","Headmaster"],
+    "New Zealand",
+    "War",
+    "1918",
+    "1949",
+    "William Te Amo",
+);
+
+const Item4 = new Item (
+    "/images/museum (4).jpg",
+    1,
+    "World War I Plaque",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Gallipoli","War"],
+    "New Zealand",
+    "War",
+    "1981",
+    "2009",
+    "Tom Smith",
+);
+
+
+const Item5 = new Item (
+    "/images/museum (5).jpg",
+    2,
+    "Stained Glass Painting",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Art","Stained glass"],
+    "New Zealand",
+    "Art",
+    "1941",
+    "1988",
+    "Frank Peters",
+);
+
+const Item6 = new Item (
+    "/images/museum (6).jpg",
+    3,
+    "D Grant, SBHS Headmaster",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["War","Headmaster"],
+    "New Zealand",
+    "War",
+    "1918",
+    "1949",
+    "William Te Amo",
+);
+const Item7 = new Item (
+    "/images/museum (7).jpg",
+    1,
+    "World War I Plaque",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Gallipoli","War"],
+    "New Zealand",
+    "War",
+    "1981",
+    "2009",
+    "Tom Smith",
+);
+
+
+const Item8 = new Item (
+    "/images/museum (8).jpg",
+    2,
+    "Stained Glass Painting",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Art","Stained glass"],
+    "New Zealand",
+    "Art",
+    "1941",
+    "1988",
+    "Frank Peters",
+);
+
+const Item9 = new Item (
+    "/images/museum (9).jpg",
+    3,
+    "D Grant, SBHS Headmaster",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["War","Headmaster"],
+    "New Zealand",
+    "War",
+    "1918",
+    "1949",
+    "William Te Amo",
+);
+const Item10 = new Item (
+    "/images/museum (10).jpg",
+    1,
+    "World War I Plaque",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Gallipoli","War"],
+    "New Zealand",
+    "War",
+    "1981",
+    "2009",
+    "Tom Smith",
+);
+
+
+const Item11 = new Item (
+    "/images/museum (11).jpg",
+    2,
+    "Stained Glass Painting",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Art","Stained glass"],
+    "New Zealand",
+    "Art",
+    "1941",
+    "1988",
+    "Frank Peters",
+);
+
+const Item12 = new Item (
+    "/images/museum (12).jpg",
+    3,
+    "D Grant, SBHS Headmaster",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["War","Headmaster"],
+    "New Zealand",
+    "War",
+    "1918",
+    "1949",
+    "William Te Amo",
+);
+
+const Item13 = new Item (
+    "/images/museum (13).jpg",
+    2,
+    "Stained Glass Painting",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Art","Stained glass"],
+    "New Zealand",
+    "Art",
+    "1941",
+    "1988",
+    "Frank Peters",
+);
+
+
+const Item14 = new Item (
+    "/images/museum (14).jpg",
+    2,
+    "Stained Glass Painting",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Art","Stained glass"],
+    "New Zealand",
+    "Art",
+    "1941",
+    "1988",
+    "Frank Peters",
+);
+
+const Item15 = new Item (
+    "/images/museum (15).jpg",
+    3,
+    "D Grant, SBHS Headmaster",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["War","Headmaster"],
+    "New Zealand",
+    "War",
+    "1918",
+    "1949",
+    "William Te Amo",
+);

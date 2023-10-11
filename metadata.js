9022af91c785347e20e057570a79b409e8cafa23
oshadhapereraSBHS <80649645+oshadhapereraSBHS@users.noreply.github.com>
2023-10-11 14:33:55 +1300

    update item date of origin

---
+++ metadata.js
@@ -0,0 +1,198 @@
+
+const Item1 = new Item (
+    "/images/museum (1).jpg",
+    1,
+    "World War I Plaque",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Gallipoli","War"],
+    "New Zealand",
+    "War",
+    "1981",
+    "2000",
+    "Tom Smith",
+);
+
+
+const Item2 = new Item (
+    "/images/museum (2).jpg",
+    2,
+    "Plaque",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Art","Stained glass"],
+    "New Zealand",
+    "Academic",
+    "1982",
+    "2001",
+    "Frank Peters",
+);
+
+const Item3 = new Item (
+    "/images/museum (3).jpg",
+    3,
+    "Signed Bat",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["War","Headmaster"],
+    "New Zealand",
+    "Sport",
+    "1983",
+    "2002",
+    "William Te Amo",
+);
+
+const Item4 = new Item (
+    "/images/museum (14).jpg",
+    4,
+    "Music Instruments",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Gallipoli","War"],
+    "New Zealand",
+    "Academic",
+    "1984",
+    "2003",
+    "Rowena Nichols",
+);
+
+
+const Item5 = new Item (
+    "/images/museum (5).jpg",
+    5,
+    "Shield",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Art","Stained glass"],
+    "New Zealand",
+    "Academic",
+    "1985",
+    "2004",
+    "Monique Barton",
+);
+
+const Item6 = new Item (
+    "/images/museum (6).jpg",
+    6,
+    "Blueprints",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["War","Headmaster"],
+    "New Zealand",
+    "Academic",
+    "1986",
+    "2005",
+    "Levi Burns",
+);
+const Item7 = new Item (
+    "/images/museum (7).jpg",
+    7,
+    "Lockers",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Gallipoli","War"],
+    "New Zealand",
+    "Academic",
+    "1987",
+    "2006",
+    "Pamela Horn",
+);
+
+
+const Item8 = new Item (
+    "/images/museum (8).jpg",
+    8,
+    "Computer",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Art","Stained glass"],
+    "New Zealand",
+    "Academic",
+    "1988",
+    "2007",
+    "Della Howell",
+);
+
+const Item9 = new Item (
+    "/images/museum (9).jpg",
+    9,
+    "Radio Transmitter",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["War","Headmaster"],
+    "New Zealand",
+    "Academic",
+    "1989",
+    "2008",
+    "Vance Mckenzie",
+);
+const Item10 = new Item (
+    "/images/museum (10).jpg",
+    10,
+    "Signed Bat 2",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Gallipoli","War"],
+    "New Zealand",
+    "Sport",
+    "1990",
+    "2009",
+    "Petra Mitchell",
+);
+
+
+const Item11 = new Item (
+    "/images/museum (11).jpg",
+    11,
+    "Uniform from WWI",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Art","Stained glass"],
+    "New Zealand",
+    "War",
+    "1991",
+    "2010",
+    "Aubrey Bishop",
+);
+
+const Item12 = new Item (
+    "/images/museum (12).jpg",
+    12,
+    "Trophies",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["War","Headmaster"],
+    "New Zealand",
+    "Academic",
+    "1992",
+    "2011",
+    "Lon Hart",
+);
+
+const Item13 = new Item (
+    "/images/museum (13).jpg",
+    13,
+    "Stained Glass Window",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Art","Stained glass"],
+    "New Zealand",
+    "Academic",
+    "1993",
+    "2012",
+    "Amado Kerr",
+);
+
+
+const Item14 = new Item (
+    "/images/museum (4).jpg",
+    14,
+    "Biology Props",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["Art","Stained glass"],
+    "New Zealand",
+    "Academic",
+    "1994",
+    "2013",
+    "Willian Hopkins",
+);
+
+const Item15 = new Item (
+    "/images/museum (15).jpg",
+    15,
+    "Books",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ["War","Headmaster"],
+    "New Zealand",
+    "Academic",
+    "1995",
+    "2014",
+    "Bert Browning",
+);

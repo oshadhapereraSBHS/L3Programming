d0674add0c51d347d2e8dcd30676c71b36dbe324
oshadhapereraSBHS <80649645+oshadhapereraSBHS@users.noreply.github.com>
2023-10-12 09:18:13 +1300

    save search term to local storage using localStorage.setItem

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const items = [];
+const searchDisplay = [];   
 
 
 class Item {
@@ -17,83 +18,15 @@ class Item {
     }
 }
 
-const Item1 = new Item (
-    "/images/museum (1).jpg",
-    1,
-    "World War I Plaque",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["Gallipoli","War"],
-    "New Zealand",
-    "War",
-    "1981",
-    "2009",
-    "Tom Smith",
-);
+function searchPage(){
+    window.location.href= "search.html";
+    localStorage.setItem("searchText",document.getElementById("searchText").value)
+}
 
-
-const Item2 = new Item (
-    "/images/museum (13).jpg",
-    2,
-    "Stained Glass Painting",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["Art","Stained glass"],
-    "New Zealand",
-    "Art",
-    "1941",
-    "1988",
-    "Frank Peters",
-);
-
-const Item3 = new Item (
-    "/images/museum (16).jpg",
-    3,
-    "D Grant, SBHS Headmaster",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["War","Headmaster"],
-    "New Zealand",
-    "War",
-    "1918",
-    "1949",
-    "William Te Amo",
-);
-
-
-window.onload = function() {
-
-    pushContent();
-    fillContents();
+function homePage(){
+    window.location.href= "index.html"
 }
 
 
-function pushContent() {
-    
-    items.push(Item1);
-    items.push(Item2);
-    items.push(Item3);
-}
-
-function fillContents() {
 
 
-    for (var i=0; i<items.length; i++) {
-        var main = document.getElementById("content");
-        var list = document.createElement("li");
-        var image = document.createElement("img");
-        var heading = document.createElement("h3");
-        var subheading = document.createElement("p");
-        var date = document.createElement("p")
-
-        main.appendChild(list);
-
-        list.appendChild(image);
-        list.appendChild(heading);
-        list.appendChild(subheading);
-        list.appendChild(date);
-    
-        image.src = items[i].source;
-        heading.innerHTML = items[i].itemName;
-        subheading.innerHTML = items[i].collection;
-        date.innerHTML = items[i].dateOfOrigin;
-    }
-    
-}

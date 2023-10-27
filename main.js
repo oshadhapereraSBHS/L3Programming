34044e6624b92b5807dffd2667ef28bdf9ecb52f
oshadhapereraSBHS <80649645+oshadhapereraSBHS@users.noreply.github.com>
2023-10-27 18:44:20 +1300

    add function check whether enter key is pressed

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const items = [];
-const searchDisplay = [];   
+const searchDisplay = [];
 
 
 class Item {
-    constructor(source, itemId, itemName, itemDescription, keywords, origin, collection, dateOfOrigin, dateDonated, donatedBy){
+    constructor(source, itemId, itemName, itemDescription, keywords, origin, collection, dateOfOrigin, dateDonated, donatedBy) {
         this.source = source;
         this.itemId = itemId;
         this.itemName = itemName;
@@ -18,13 +18,67 @@ class Item {
     }
 }
 
-function searchPage(){
-    window.location.href= "search.html";
+function searchPage() {
+    window.location.href = "search.html";
+
     var intialSearchText = document.getElementById("searchText").value;
     var finalSeachText = intialSearchText.toLowerCase();
     localStorage.setItem("searchText", finalSeachText)
+
+
+
+
+
 }
 
-function homePage(){
-    window.location.href= "index.html"
+function homePage() {
+    window.location.href = "index.html"
 }
+
+
+function checkKeyPress(event) {
+    if (event.keyCode === 13) {
+        //event.preventDefault();
+        searchPage();
+    }
+}
+// const filters = [];
+
+// function displayArrayInContainer(data, containerId, arrayName) {
+//     const container = document.getElementById(containerId);
+//     container.style.fontWeight = 'bold';
+
+//     for (var i = 0; i < data.length; i++) {
+//         var checkbox = document.createElement("input");
+//         checkbox.type = "checkbox";
+
+//         checkbox.setAttribute("data-array-name", arrayName);
+//         checkbox.setAttribute("data-array-item", data[i]);
+
+//         var resultItem = document.createElement("label");
+//         resultItem.classList.add('p-text');
+
+//         resultItem.innerHTML = data[i];
+
+//         var lineBreak = document.createElement("br");
+//         container.appendChild(lineBreak);
+
+//         container.appendChild(checkbox);
+//         container.appendChild(resultItem);
+
+
+//         checkbox.addEventListener("click", function () {
+//             var arrayIndex = this.getAttribute("data-array-name");
+//             var arrayItem = this.getAttribute("data-array-item");
+
+
+//             const nestedFilters = [arrayIndex, arrayItem];
+
+//             filters.push(nestedFilters);
+
+//         });
+
+
+//     }
+
+// }

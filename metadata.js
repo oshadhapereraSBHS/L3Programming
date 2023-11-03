822950a6ea3c77dadfbb50b4d9c4fc414e1dc540
oshadhapereraSBHS <80649645+oshadhapereraSBHS@users.noreply.github.com>
2023-11-03 16:53:06 +1300

    check if any item properties match current filter

--- metadata.js
+++ metadata.js
@@ -1,24 +1,37 @@
+//This page contains the museum collection items defined as objects. Metadata are added as object properties.
 
-const Item1 = new Item (
+//Item 1
+const Item1 = new Item(
+    //source
     "/images/museum (1).jpg",
+    //ID
     1,
+    //Title
     "World War I Plaque",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["Dardanelles","War"],
+    //Description
+    "War memorial commemorating Gallipoli Dardanelles Campaign",
+    //Keywords
+    ["Dardanelles", "War"],
+    //Country of origin
     "New Zealand",
+    //Collection
     "War",
+    //Date of origin
     "1981",
+    //Date of donation
     "2000",
+    //Donated by
     "Tom Smith",
 );
+//Above format is repeated for each item below
 
 
-const Item2 = new Item (
+const Item2 = new Item(
     "/images/museum (2).jpg",
     2,
     "Plaque",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["Commemoration","Plaque"],
+    "Plaque commemorating academic event",
+    ["Commemoration", "Plaque"],
     "New Zealand",
     "Academic",
     "1982",
@@ -26,12 +39,12 @@ const Item2 = new Item (
     "Frank Peters",
 );
 
-const Item3 = new Item (
+const Item3 = new Item(
     "/images/museum (3).jpg",
     3,
     "Signed Bat",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["Cricket","Bat"],
+    "Signed cricked bat display",
+    ["Cricket", "Bat"],
     "New Zealand",
     "Sport",
     "1983",
@@ -39,12 +52,12 @@ const Item3 = new Item (
     "William Te Amo",
 );
 
-const Item4 = new Item (
+const Item4 = new Item(
     "/images/museum (14).jpg",
     4,
     "Music Instruments",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["Music","Drums"],
+    "Music instruments including drums and trumpets",
+    ["Music", "Drums"],
     "New Zealand",
     "Academic",
     "1984",
@@ -53,12 +66,12 @@ const Item4 = new Item (
 );
 
 
-const Item5 = new Item (
+const Item5 = new Item(
     "/images/museum (5).jpg",
     5,
     "Shield",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["Plaque","Shield"],
+    "Shield celebrating event",
+    ["Plaque", "Shield"],
     "New Zealand",
     "Academic",
     "1985",
@@ -66,24 +79,24 @@ const Item5 = new Item (
     "Monique Barton",
 );
 
-const Item6 = new Item (
+const Item6 = new Item(
     "/images/museum (6).jpg",
     6,
     "Blueprints",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["Plan","Blueprint"],
+    "Southland Boys High School architect blueprints",
+    ["Plan", "Blueprint"],
     "New Zealand",
     "Academic",
     "1986",
     "2005",
     "Levi Burns",
 );
-const Item7 = new Item (
+const Item7 = new Item(
     "/images/museum (7).jpg",
     7,
     "Lockers",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["Locker","Cupboard"],
+    "Lockers containing various items",
+    ["Locker", "Cupboard"],
     "New Zealand",
     "Academic",
     "1987",
@@ -92,12 +105,12 @@ const Item7 = new Item (
 );
 
 
-const Item8 = new Item (
+const Item8 = new Item(
     "/images/museum (8).jpg",
     8,
     "Computer",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["Computer","Technology"],
+    "Computer and technology equipment",
+    ["Computer", "Technology"],
     "New Zealand",
     "Academic",
     "1988",
@@ -105,24 +118,24 @@ const Item8 = new Item (
     "Della Howell",
 );
 
-const Item9 = new Item (
+const Item9 = new Item(
     "/images/museum (9).jpg",
     9,
     "Radio Transmitter",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["Radio","Transmitter"],
+    "Radio transmitter and headphones",
+    ["Radio", "Transmitter"],
     "New Zealand",
     "Academic",
     "1989",
     "2008",
     "Vance Mckenzie",
 );
-const Item10 = new Item (
+const Item10 = new Item(
     "/images/museum (10).jpg",
     10,
     "Signed Bat 2",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["Bat","Cricket"],
+    "Signed cricket bat display",
+    ["Bat", "Cricket"],
     "New Zealand",
     "Sport",
     "1990",
@@ -131,12 +144,12 @@ const Item10 = new Item (
 );
 
 
-const Item11 = new Item (
+const Item11 = new Item(
     "/images/museum (11).jpg",
     11,
     "Uniform from WWI",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["Pants","Gallipolli"],
+    "Uniform worn during World War I",
+    ["Uniform", "Gallipolli"],
     "New Zealand",
     "War",
     "1991",
@@ -144,12 +157,12 @@ const Item11 = new Item (
     "Aubrey Bishop",
 );
 
-const Item12 = new Item (
+const Item12 = new Item(
     "/images/museum (12).jpg",
     12,
     "Trophies",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["Achievement","Trophy"],
+    "Academic and sporting trophy display",
+    ["Achievement", "Trophy"],
     "New Zealand",
     "Academic",
     "1992",
@@ -157,12 +170,12 @@ const Item12 = new Item (
     "Lon Hart",
 );
 
-const Item13 = new Item (
+const Item13 = new Item(
     "/images/museum (13).jpg",
     13,
     "Stained Glass Window",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["Art","Stained glass"],
+    "Stained glass window featuring owl",
+    ["Art", "Stained glass"],
     "New Zealand",
     "Academic",
     "1993",
@@ -171,12 +184,12 @@ const Item13 = new Item (
 );
 
 
-const Item14 = new Item (
+const Item14 = new Item(
     "/images/museum (4).jpg",
     14,
     "Biology Props",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["Clock","Human"],
+    "Human body systems model , clock and camera",
+    ["Clock", "Human"],
     "New Zealand",
     "Academic",
     "1994",
@@ -184,16 +197,15 @@ const Item14 = new Item (
     "Willian Hopkins",
 );
 
-const Item15 = new Item (
+const Item15 = new Item(
     "/images/museum (15).jpg",
     15,
     "Books",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    ["Book","History"],
+    "Cupboard of old books",
+    ["Book", "History"],
     "New Zealand",
     "Academic",
     "1995",
     "2014",
     "Bert Browning",
 );
-
